ba39ceb4e440ba3365500eac27fce66b646fc7cb
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-04-02 20:12:17 -0300

    feat: introdução aos services
    Introdução a criação de services no angular, além do uso de conceitos como injeção de dependência

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -7,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
   nomePortal: string;
-  cursos: string[] = ['Java', 'Angular', 'Ext JS'];
+  cursos: string[];
 
-  constructor() {
-    this.nomePortal = 'https://loiane.training'
+  constructor(private cursoService: CursosService) {
+    this.nomePortal = 'https://loiane.training';
+    this.cursos = this.cursoService.getCursos();
   }
 
   ngOnInit() {}
