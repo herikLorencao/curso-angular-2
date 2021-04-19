4639e360512e10fe42fd73cc5e3fbd7186a6840a
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-04-19 20:27:55 -0300

    feat: melhorando captura de parametro com subject
    Utilizando subject para captura do paramêtro durante a mudança. Utilização do Subscription do RxJs e life-cycle hooks do Angular

--- roteamento/src/app/curso-detalhe/curso-detalhe.component.ts
+++ roteamento/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Subscription } from 'rxjs/index';
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
   styleUrls: ['./curso-detalhe.component.scss'],
 })
-export class CursoDetalheComponent implements OnInit {
+export class CursoDetalheComponent implements OnInit, OnDestroy {
   id: string;
+  idInscription: Subscription;
 
   constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id'];
+    // this.id = this.route.snapshot.params['id'];
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.idInscription = this.route.params.subscribe(
+      (param: any) => (this.id = param['id'])
+    );
+  }
+
+  ngOnDestroy() {
+    this.idInscription.unsubscribe();
+  }
 }
