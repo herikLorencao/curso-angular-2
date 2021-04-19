0c5e468913d853998a42650b276aef6bb1041897
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-04-19 20:13:25 -0300

    feat: capturando parametros da rota
    Capturando parâmetros de uma rota utilizando o ActivatedRoute

--- roteamento/src/app/app.module.ts
+++ roteamento/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, CursosComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, CursosComponent, CursoDetalheComponent],
   imports: [BrowserModule, AppRoutingModule /*, routing*/],
   providers: [],
   bootstrap: [AppComponent],
