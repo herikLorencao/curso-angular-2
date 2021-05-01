6147ebf047ce683cfb5e4e8042ed12a2314520a2
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-05-01 16:51:33 -0300

    feat: adicionando campos do formulario
    Adicionando campos no formulário reativo com formBuilder, FormGroup e FormControl

--- forms/src/app/data-driven/data-driven.component.ts
+++ forms/src/app/data-driven/data-driven.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven',
   templateUrl: './data-driven.component.html',
-  styleUrls: ['./data-driven.component.scss']
+  styleUrls: ['./data-driven.component.scss'],
 })
 export class DataDrivenComponent implements OnInit {
+  formulario: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-  }
+    // this.formulario = new FormGroup({
+    //   nome: new FormControl(null),
+    //   email: new FormControl(null),
+    // });
 
+    this.formulario = this.formBuilder.group({
+      nome: [null],
+      email: [null],
+    });
+  }
 }
