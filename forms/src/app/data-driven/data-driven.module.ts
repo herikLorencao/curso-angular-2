3cc7876d194f02076211d7c696bcd1ed3d532629
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-05-01 16:57:17 -0300

    feat: adicionando estrutura inicial do html para form
    Adicionando estrutura inicial do componente html

--- forms/src/app/data-driven/data-driven.module.ts
+++ forms/src/app/data-driven/data-driven.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DataDrivenComponent } from './data-driven.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [DataDrivenComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, SharedModule],
 })
 export class DataDrivenModule {}
--- forms/src/app/template-driven/template-driven.module.ts
+++ forms/src/app/template-driven/template-driven.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 import { TemplateDrivenComponent } from './template-driven.component';
-import { FormDebugComponent } from '../form-debug/form-debug.component';
 @NgModule({
-  declarations: [TemplateDrivenComponent, FormDebugComponent],
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  declarations: [TemplateDrivenComponent],
+  imports: [CommonModule, FormsModule, HttpClientModule, SharedModule],
   exports: [],
 })
 export class TemplateDrivenModule {}
