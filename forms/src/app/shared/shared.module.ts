3ba1ae3e2d41e1c511ce5ea9b760964b855ef6a5
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-05-06 20:27:01 -0300

    feat: adicionando campo de input personalizado
    Adicionando campo de input personalizado a partir da interface ControlValueAccessor

--- forms/src/app/shared/shared.module.ts
+++ forms/src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormDebugComponent } from './form-debug/form-debug.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ErrorMsgComponent } from './error-msg/error-msg.component';
+import { InputFieldComponent } from './input-field/input-field.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [FormDebugComponent, ErrorMsgComponent],
-  imports: [CommonModule, HttpClientModule],
-  exports: [FormDebugComponent, ErrorMsgComponent],
+  declarations: [FormDebugComponent, ErrorMsgComponent, InputFieldComponent],
+  imports: [CommonModule, HttpClientModule, FormsModule],
+  exports: [FormDebugComponent, ErrorMsgComponent, InputFieldComponent],
 })
 export class SharedModule {}
