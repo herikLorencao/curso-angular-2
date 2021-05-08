b0da4e7590062d0399069ff570f651c7479a8c12
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-05-08 20:27:11 -0300

    feat: utilizando variaveis de ambiente
    Utilizando varíavel de ambiente para isolar a URL de acesso ao backend

--- requests-http/src/app/cursos/cursos.service.ts
+++ requests-http/src/app/cursos/cursos.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Curso } from '../typings/curso';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CursosService {
-  private readonly API = 'http://localhost:3000/cursos';
+  private readonly API = `${environment.api}/cursos`;
 
   constructor(private http: HttpClient) {}
 
