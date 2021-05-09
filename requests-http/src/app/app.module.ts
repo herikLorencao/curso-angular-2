91b3d78dc2c53a15051a7381e882e53390130833
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-05-09 17:15:56 -0300

    feat: adicionando alert para mensagens de erro

--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -2,12 +2,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ModalModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { EMPTY, Observable, Subject } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { AlertComponent } from 'src/app/shared/alert/alert.component';
 import { Curso } from 'src/app/typings/curso';
 import { CursosService } from '../cursos.service';
 
@@ -13,8 +15,12 @@ export class CursosListaComponent implements OnInit {
   // cursos: Curso[];
   cursos$: Observable<Curso[]>;
   error$ = new Subject<boolean>();
+  bsModalRef: BsModalRef;
 
-  constructor(private service: CursosService) {}
+  constructor(
+    private service: CursosService,
+    private modalService: BsModalService
+  ) {}
 
   ngOnInit(): void {
     // this.service.list().subscribe((dados) => (this.cursos = dados));
@@ -26,8 +32,11 @@ export class CursosListaComponent implements OnInit {
       // tap(),
       // map(),
       catchError((error) => {
-        console.error(error);
-        this.error$.next(true);
+        // console.error(error);
+        // this.error$.next(true);
+        this.handleError(
+          'Erro no carregamento dos cursos. Tente novamente mais tarde.'
+        );
         return EMPTY;
       })
     );
@@ -36,5 +45,11 @@ export class CursosListaComponent implements OnInit {
     //   dados => this.cursos = dados,
     //   error => console.log(error),
     // )
+  }
+
+  handleError(message: string) {
+    this.bsModalRef = this.modalService.show(AlertComponent);
+    this.bsModalRef.content.message = message;
+    this.bsModalRef.content.type = 'danger';
   }
 }
